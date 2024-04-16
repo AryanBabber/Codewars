82831ce5c3ac6d698dfa3f25ed5ad8e661054eed
Aryan Babber <aryanbabber12@gmail.com>
2024-04-16 05:46:03 +0530

    today's challenges

---
+++ 5-kyu/Directions Reduction.js
@@ -0,0 +1,6 @@
+function dirReduc(arr) {
+	let str = arr.join(""),
+		pattern = /NORTHSOUTH|EASTWEST|SOUTHNORTH|WESTEAST/;
+	while (pattern.test(str)) str = str.replace(pattern, "");
+	return str.match(/(NORTH|SOUTH|EAST|WEST)/g) || [];
+}
---
+++ 8-kyu/Sum Arrays.js
@@ -0,0 +1,6 @@
+// Sum Numbers
+function sum(numbers) {
+	"use strict";
+
+	return numbers.length == 0 ? 0 : numbers.reduce((a, b) => a + b, 0);
+}

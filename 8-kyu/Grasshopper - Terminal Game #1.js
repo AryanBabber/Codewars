402c32fd8bc15a3db836349ce1feff21afad9755
Aryan Babber <aryanbabber16@gmail.com>
2024-07-12 12:25:38 +0530

    today's challenges

---
+++ 8-kyu/Grasshopper - Terminal Game #1.js
@@ -0,0 +1,15 @@
+class Hero {
+	constructor(
+		name = "Hero",
+		experience = 0,
+		health = 100,
+		position = "00",
+		damage = 5
+	) {
+		this.name = name;
+		this.experience = experience;
+		this.health = health;
+		this.position = position;
+		this.damage = damage;
+	}
+}

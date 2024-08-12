6b189d6051e16dfa1b2ecb008bfd901ddf9c116b
Aryan Babber <aryanbabber16@gmail.com>
2024-08-12 06:51:21 +0530

    today's challenges

--- 7-kyu/Count the divisors of a number.js
+++ 7-kyu/Count the divisors of a number.js
@@ -1,9 +1,11 @@
 function getDivisorsCnt(n) {
-	let arr = [];
-
-	for (let i = 1; i <= n; i++) {
-		if (n % i == 0) arr.push(i);
+	let num = 0;
+	if (n == 1) return 1;
+	if (n % Math.sqrt(n) == 0) num++;
+	for (let i = 1; i < Math.sqrt(n); i++) {
+		if (n % i == 0) {
+			num += 2;
+		}
 	}
-
-	return arr.length;
+	return num;
 }

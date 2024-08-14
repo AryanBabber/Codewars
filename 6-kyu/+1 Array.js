b48b19b9c41dc867b9e4bcf7fd8a8d80a559cbe3
Aryan Babber <aryanbabber16@gmail.com>
2024-08-14 06:13:02 +0530

    today's challenges

--- 6-kyu/+1 Array.js
+++ 6-kyu/+1 Array.js
@@ -1,14 +1,14 @@
 function upArray(arr) {
-	if (arr.length === 0) return null;
-	let carry = 1;
-	for (let i = arr.length - 1; i >= 0; i--) {
-		if (arr[i] > 9 || arr[i] < 0) return null;
-		if (carry > 0) {
-			arr[i] += carry;
-			carry = Math.floor(arr[i] / 10);
-			arr[i] = arr[i] % 10;
-		}
-	}
-	if (carry > 0) arr.unshift(carry);
+	const verifyArr = arr.filter((number) => number >= 0 && number < 10);
+
+	if (arr.length !== verifyArr.length || arr.length === 0) return null;
+
+	let i = arr.length - 1;
+
+	while (i >= 0 && arr[i] == 9) arr[i--] = 0;
+
+	if (i < 0) arr.unshift(1);
+	else arr[i]++;
+
 	return arr;
 }

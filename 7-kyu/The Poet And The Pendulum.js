a8c64e4876ec486cee0f04114a0d69113e4f0900
Aryan Babber <aryanbabber16@gmail.com>
2024-12-02 00:26:22 +0530

    today's challenges

---
+++ 7-kyu/The Poet And The Pendulum.js
@@ -0,0 +1,8 @@
+function pendulum(values) {
+	let arr1 = [],
+		arr2 = [];
+	const sorted = values
+		.sort((a, b) => a - b)
+		.map((v, i) => (i % 2 === 0 ? arr1 : arr2).push(v));
+	return arr1.reverse().concat(arr2);
+}

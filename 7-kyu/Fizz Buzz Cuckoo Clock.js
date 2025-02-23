c9eff98d5ec0d3632dd144e95cdf8d9ac96be8a1
Aryan Babber <aryanbabber16@gmail.com>
2025-02-23 10:11:28 +0530

    today's challenges

--- 7-kyu/Fizz Buzz Cuckoo Clock.js
+++ 7-kyu/Fizz Buzz Cuckoo Clock.js
@@ -1,7 +1,6 @@
 function fizzBuzzCuckooClock(time) {
-	let a = time.split(":").map((v) => parseInt(v));
-	let m = a[1],
-		h = a[0];
+	let [h, m] = time.split(":").map((v) => parseInt(v));
+
 	h = h === 0 ? 12 : h > 12 ? h - 12 : h;
 	if (m === 0) return "Cuckoo ".repeat(h).trim();
 	if (m === 30) return "Cuckoo";
